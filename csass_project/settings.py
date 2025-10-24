62d5a4abff5488f01ee980a242c80f71a5a03c7a
Patrick Odeh <vyltyan@gmail.com>
2025-10-24 06:22:02 +0100

    railway

--- csass_project/settings.py
+++ csass_project/settings.py
@@ -8,6 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-temp-key')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = [
+    'csass-production.up.railway.app',
+    'localhost',
     'booking.revenueaccelerationunit.com',
 ]
 
